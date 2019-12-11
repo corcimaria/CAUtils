41d0354caaff3e55c5d4434fa82d2eb094d80c00
acorcimari <acorcimari@universoft.tech>
2019-12-11 10:58:30 +0200

    test

--- Sources/CAUtils/CAUtils.swift
+++ Sources/CAUtils/CAUtils.swift
@@ -1,3 +1,19 @@
-struct CAUtils {
+/*struct CAUtils {
     var text = "Hello, World!"
+}*/
+
+protocol Iconnizable {
+    var ok: String { get }
+    var error: String { get }
+    var page: String { get }
+    var warning: String { get }
+    var clock: String { get }
+}
+
+extension String:Iconnizable {
+    var ok: String { return self+" 🍏"}
+    var error: String { return self+" 🍎"}
+    var page:String { return self+" ☀️" }
+    var warning:String { return self+" ⚠️" }
+    var clock:String { return self+" ⏱" }
 }
